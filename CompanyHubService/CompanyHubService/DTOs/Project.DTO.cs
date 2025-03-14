ba326c7f7028cde89abc1208ed2383b398773d84
BartuAlb <bartu.albayrak@ug.bilkent.edu.tr>
2025-03-14 14:48:47 +0300

    comment

--- CompanyHubService/CompanyHubService/DTOs/Project.DTO.cs
+++ CompanyHubService/CompanyHubService/DTOs/Project.DTO.cs
@@ -7,15 +7,15 @@ namespace CompanyHubService.DTOs
     {
         public Guid ProjectId { get; set; }
 
-        [JsonPropertyName("project_name")]  // Explicitly map JSON field
+        //[JsonPropertyName("project_name")]  // Explicitly map JSON field
         public string ProjectName { get; set; }
         public string Description { get; set; }
 
-        [JsonPropertyName("technologies_used")]  // Explicitly map JSON field
+        //[JsonPropertyName("technologies_used")]  // Explicitly map JSON field
         public List<string> TechnologiesUsed { get; set; } // List for easier input
         public string Industry { get; set; }
 
-        [JsonPropertyName("client_type")]  // Explicitly map JSON field
+        //[JsonPropertyName("client_type")]  // Explicitly map JSON field
         public string ClientType { get; set; }
         public string Impact { get; set; }
         public DateTime StartDate { get; set; }
@@ -29,7 +29,7 @@ namespace CompanyHubService.DTOs
         public CompanyDTO ClientCompany { get; set; }
         public CompanyDTO ProviderCompany { get; set; }
 
-        [JsonPropertyName("project_url")]  // Explicitly map JSON field
+        //[JsonPropertyName("project_url")]  // Explicitly map JSON field
         public string ProjectUrl { get; set; }
 
         public CompanyDTO Company { get; set; }
