cea6df93ad0724b65ebb9a77534a47b2c168291f
BartuAlb <bartu.albayrak@ug.bilkent.edu.tr>
2025-04-10 11:17:08 +0300

    logo fix

---
+++ CompanyHubService/CompanyHubService/Migrations/20250410075226_AddCompanyLogo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyHubService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyLogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Companies");
+        }
+    }
+}
