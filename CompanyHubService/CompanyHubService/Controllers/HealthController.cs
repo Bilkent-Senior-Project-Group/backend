0bf0798e5d440ce181d8f604893d4d9fa8ab2370
Yaşar <yasar.tatlicioglu@ug.bilkent.edu.tr>
2025-04-24 13:29:23 +0300

    health check added for production environment

---
+++ CompanyHubService/CompanyHubService/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyHubService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Service is healthy");
+        }
+    }
+}
